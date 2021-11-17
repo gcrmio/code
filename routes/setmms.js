975359a8328d2012b57abf718a697e8bf15d2853
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-17 20:11:41 +0900

    V02

--- routes/setmms.js
+++ routes/setmms.js
@@ -78,19 +78,19 @@ let qry3 =  `INSERT INTO targets
 
 
 let qry4 =  `INSERT INTO contents	
-                SELECT  id_msg_id 	msg_id,
-                        max(id_display_subject) msg_subject,
-                        max(id_display_body) msg_body_text,
-                        max(id_display_ctsr) msg_body_content,
-                        max(case when position('Image' in id_load_content_type)>0 then 'IMAGE' else 'TEXT' end) msg_type,
-                        max(id_load_content_type) content_src,
-                        (case when id_display_subject = '' then 0 else length(max(id_display_subject)) end) msg_subject_length,
-                        (case when id_display_body = '' then 0 else length(max(id_display_body)) end) msg_body_length,
-                        (case when id_display_ctsr_size = '' then 0 else to_number(id_display_ctsr_size,'999999999') end) msg_content_size,
-                        to_char(now(), 'YYYY-MM-DD HH24:MI:SS') set_date
-                FROM msg_working
-                WHERE proc_yn='N'
-                GROUP BY id_msg_id;`;      
+                    SELECT id_msg_id 	msg_id, 
+                    max(id_display_subject) 	msg_subject, 
+                    max(id_display_body) 	msg_body_text, 
+                    max(id_display_ctsr)     msg_body_content, 
+                    max(case when position('image' in id_load_content_type)>0 then 'IMAGE' else 'TEXT' end) msg_type, 
+                    max(id_load_content_type) content_src, 
+                    length(case when id_display_subject = '' then NULL else max(id_display_subject) end) msg_subject_length, 
+                    length(case when id_display_body = '' then NULL else max(id_display_body) end) msg_body_length, 
+                    max(case when id_display_ctsr_size = '' then NULL else to_number(id_display_ctsr_size,'999999999') end) msg_content_size, 
+                    to_char(now(), 'YYYY-MM-DD HH24:MI:SS') set_date
+                    FROM msg_working 
+                    WHERE proc_yn='N'
+                    GROUP BY id_msg_id, id_display_subject, id_display_body, id_display_ctsr_size`;      
 
 
 // let qry5 =  " insert into transmit "
