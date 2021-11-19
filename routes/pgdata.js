e751b44806555b90ffc507fc2cfdb5ac174a0625
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-19 17:53:11 +0900

    V00

--- routes/pgdata.js
+++ routes/pgdata.js
@@ -49,7 +49,7 @@ module.exports.getcredit = function (req, res){
       }
     };
     request(options, function (error, res) {
-        return res; 
+        console.log("response: "+res); 
     });
     res.status(200);
 }
