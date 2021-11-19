a14d7319e9c90b09c8364a52d9432f8d6e812be3
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-19 18:00:14 +0900

    V00

--- routes/pgdata.js
+++ routes/pgdata.js
@@ -34,24 +34,22 @@ pool.query('INSERT INTO mms_list VALUES( $1, $2, $3)', ['10','999','MMM'], (err,
   });
 */
 module.exports.getcredit = function (req, res){
+    const url = 'https://oms.every8d.com/API21/HTTP/getCredit.ashx';
     const uid = process.env.Euid;
     const password = process.env.Epassword;
-
-    var options = {
-      'method': 'GET',
-      'url': 'https://oms.every8d.com/getCredit.ashx',
-      'headers': {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      form: {
-        'UID': process.env.Euid,
-        'PWD': process.env.Epassword
-      }
-    };
-    request(options, function (error, res) {
-        console.log("response: "+res); 
-    });
-    res.status(200);
+  
+    var geturl = url+'?UID='+uid+'&PWD='+password;
+    console.log(geturl);
+    console.log('======================');
+    request.get({
+        url: geturl
+    }, function(error, response, html){
+        if(error){
+            console.log(error);
+        }
+        console.log('Received Server Data!');
+        console.log(html);
+    })
 }
 
 module.exports.getmsgid = function (req, res) {
