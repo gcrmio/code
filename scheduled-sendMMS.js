8260e6b9a2a05504cf7c33997b78855589900906
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-23 16:03:36 +0900

    V00

--- scheduled-sendMMS.js
+++ scheduled-sendMMS.js
@@ -1,5 +1,47 @@
 "use strict";
 var sendMMS     = require('./sendMMS');
 
-//sendMMS.dbSelect();
-//console.log('APP02 FINISHED =============================================');
+"use strict";
+var setMMS = require('./routes/setmms');
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+    host:       process.env.PG_host,
+    user:       process.env.PG_user,
+    password:   process.env.PG_password,  
+    database:   process.env.PG_database,
+    port:       process.env.PG_port,
+    ssl: { rejectUnauthorized: false },
+});
+
+let qry1 = `SELECT app_name, status, last_run_date FROM scheduler WHERE app_name = 'app02'`;
+let qry2 = `UPDATE scheduler SET status = 'OFF', last_run_date = to_char(now() at time zone 'KST', 'YYYY-MM-DD HH24:MI:SS') WHERE app_name = 'app02'`;
+let qry3 = `UPDATE scheduler SET status = 'ON', last_run_date = to_char(now() at time zone 'KST', 'YYYY-MM-DD HH24:MI:SS') WHERE app_name = 'app02'`;
+
+module.exports.getResult = function (req, res) {    
+    pool
+	.query(qry1)
+	.then(res => {
+		console.log('11111');
+		var result = result.rows;
+		if(result['status'] = 'ON'){
+			pool
+			.query(qry2)
+			.then(res => {  
+				console.log('22222');
+				sendMMS.dbSelect();
+				pool
+					.query(qry3)
+					.then(res => {  
+						console.log('APP02 FINISHED =============================================');                                
+					}) 
+					.catch(err => console.error('Error executing query', err.stack))        
+			}) 
+			.catch(err => console.error('Error executing query', err.stack))
+		}
+		else {
+			console.log('App02 is still running');
+		}
+	}) 
+	.catch(err => console.error('Error executing query', err.stack));
+}
--- scheduled-setMMS.js
+++ scheduled-setMMS.js
@@ -11,12 +11,34 @@ const pool = new Pool({
     ssl: { rejectUnauthorized: false },
 });
 
-let qry = `SELECT app_name, status, last_run_date FROM scheduler WHERE app_name = 'app01'`
-module.exports.getResult = function (req, res) {
-    pool.query(qry, (err, res) => {
-        console.log(res);
-    })
-}
+let qry1 = `SELECT app_name, status, last_run_date FROM scheduler WHERE app_name = 'app01'`;
+let qry2 = `UPDATE scheduler SET status = 'OFF', last_run_date = to_char(now() at time zone 'KST', 'YYYY-MM-DD HH24:MI:SS') WHERE app_name = 'app01'`;
+let qry3 = `UPDATE scheduler SET status = 'ON', last_run_date = to_char(now() at time zone 'KST', 'YYYY-MM-DD HH24:MI:SS') WHERE app_name = 'app01'`;
 
-//setMMS.setMMS();
-//console.log('APP01 FINISHED =============================================');
+module.exports.getResult = function (req, res) {    
+    pool
+	.query(qry1)
+	.then(res => {
+		console.log('11111');
+		var result = result.rows;
+		if(result['status'] = 'ON'){
+			pool
+			.query(qry2)
+			.then(res => {  
+				console.log('22222');
+				setMMS.setMMS();
+				pool
+					.query(qry3)
+					.then(res => {  
+						console.log('APP01 FINISHED =============================================');                                
+					}) 
+					.catch(err => console.error('Error executing query', err.stack))        
+			}) 
+			.catch(err => console.error('Error executing query', err.stack))
+		}
+		else {
+			console.log('App01 is still running');
+		}
+	}) 
+	.catch(err => console.error('Error executing query', err.stack));
+}
