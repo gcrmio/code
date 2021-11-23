e0d11fc5239ea9599f027d8454ab7d83bbe2d17b
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-23 10:27:11 +0900

    V00

--- scheduled-sendMMS.js
+++ scheduled-sendMMS.js
@@ -1,10 +1,5 @@
 "use strict";
-var express     = require('express');
 var sendMMS     = require('./sendMMS');
-var app = express();
 
-function executeApp02() {
-    sendMMS.dbSelect();
-    console.log('APP02 FINISHED =============================================');
-}
-executeApp02();
+sendMMS.dbSelect();
+console.log('APP02 FINISHED =============================================');
--- scheduled-setMMS.js
+++ scheduled-setMMS.js
@@ -1,10 +1,5 @@
 "use strict";
-var express     = require('express');
 var setMMS = require('./routes/setmms');
-var app = express();
 
-function executeApp01() {
-    setMMS.setMMS();
-    console.log('APP01 FINISHED =============================================');
-}
-executeApp01();
+setMMS.setMMS();
+console.log('APP01 FINISHED =============================================');
