ae93f25726a1b34458719f488305ef3e3f3e741c
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-23 19:09:42 +0900

    V00

---
+++ schedulerRender.js
@@ -0,0 +1,24 @@
+const {Pool, Client} = require('pg');
+
+const pool = new Pool({
+  host: process.env.PG_host,
+  user: process.env.PG_user,
+  password: process.env.PG_password,
+  database: process.env.PG_database,
+  port: process.env.PG_port,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports.viewStatus = function(req, res){
+    let query = 'SELECT * FROM scheduler';
+    pool.query(query, (err, result) => {
+        if(err){
+            console.log('query error: ' + err);
+            res.status(404);
+        }
+        return res.status(200).json(JSON.stringify(res.rows)); 
+    })
+    result.status(200);
+}
