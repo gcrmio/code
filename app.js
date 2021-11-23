e1815cd7b661d18ad7c8f847439025696defc24c
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-23 20:54:32 +0900

    V02

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.get('/sendMMS', (req, res) => {
   }
 })
 
-app.post('/scheduler', scheduler.viewStatus);
+app.get('/scheduler', scheduler.viewStatus);
 
 // app.get('/scheduler01', (req, res) => {
 //   scheduledSetMMS.getResult();
