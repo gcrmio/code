2d94ffbd2f2fa422e5cb2c3782415ce5ce1c9699
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-25 13:26:28 +0900

    V03

--- receiveResult.js
+++ receiveResult.js
@@ -107,7 +107,7 @@ function resultSMS(batch_id, msg_id){
 function updateTransmit(sms_mobile, sms_send_time, sms_status, msg_id){
     console.log('sms_mobile= '+sms_mobile);
     // var phone_no = sms_mobile;
-    var phone_no = sms_mobile.startsWith('+886')? sms_mobile.replace('+886','') : sms_mobile;
+    var phone_no = sms_mobile.startsWith('+886')? sms_mobile.replace('+886','0') : sms_mobile;
     console.log('phone_no= '+phone_no);
     var send_date = sms_send_time;
     // var success_yn = sms_status="100"? "S":"F";
