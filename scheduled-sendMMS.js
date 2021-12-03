0b411a0235500f43344f45af939d6680b55db896
wjchoi <wonjeung.choi@amorepacific.com>
2021-12-03 13:38:17 +0900

    V00

--- scheduled-sendMMS.js
+++ scheduled-sendMMS.js
@@ -14,6 +14,7 @@ const pool = new Pool({
 });
 
 let qry1 = `SELECT app_name, status, last_run_date FROM scheduler WHERE app_name = 'app02'`;
+let qry0 = `SELECT app_name, status, last_run_date FROM scheduler WHERE app_name = 'app01'`;
 let qry2 = `UPDATE scheduler SET status = 'OFF', last_run_date = to_char(now() at time zone 'KST', 'YYYY-MM-DD HH24:MI:SS') WHERE app_name = 'app02'`;
 let qry3 = `UPDATE scheduler SET status = 'ON', last_run_date = to_char(now() at time zone 'KST', 'YYYY-MM-DD HH24:MI:SS') WHERE app_name = 'app02'`;
 //module.exports.getResult = function (req, res) {    
